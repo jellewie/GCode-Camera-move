cd430788bd51cf81adadffb9145d8d7c6f96ef5a
jellewie <1545223+jellewie@users.noreply.github.com>
2019-07-02 15:00:29 +0200

    Added variables
    So people would be able to edit them quickly

--- CameraScript.py
+++ CameraScript.py
@@ -17,29 +17,36 @@ OutFile=open("C_" + inpfilename,"w")			##Create output file
 ReturnCoordsFlag=0								##Reset the flag that markes that we have coords to return to
 
 
+Feedrate = 6000		##mm/min speed 
+FeedrateSlow = 300	##mm/min speed to move slowly
+PosX = 10			##X pos to go to when pausing
+PosY = 235			##Y pos to go to when pausing
+Zhop = 1			##Amount to Zhop 
+Retract = 8			##Distance to retract
+PosSlowY = 5 		##Amount to slowly move of the PosY to pres the button
 
 while True:
-	line=file.readline()
-	outfile.write(line)
-	line=line.rstrip()
-	if line==";End of Gcode": break						##Stop if we find this line
-	content=line.split()
-	try: tester=content[0]
-	except: continue
-	if content[0]=="G0":
-		coords=line
-	if re.search("^;LAYER:",line):														##This would indicate a layer change
-		outfile.write("G91\n" + 		##Use relative Positioning
-		"G1 F6000 E-8\n" + 				##Pull in filement
-		"G1 F6000 Z1\n" + 				##Do a Z-hop
-		"G90\n" + 						##Use Absolute Positioning
-		"G1 F6000 X10 Y230\n" + 		##Move almost to the edge
-		"G1 F300 Y235\n" + 				##Move (slowly) and push the button
-		"G0 F6000\n")					##Set the feedrate back (else the code seems to go slow??) NEEDS TO BE CHECKED
-		outfile.write("G91\n" + 		##Use relative Positioning
-		"G1 F6000 E8\n" + 				##Z-hop back
-		"G1 F6000 Z-1\n" + 				##Undo the Z hop
-		"G90\n" )						##Use Absolute Positioning
+	line=InFile.readline()						##Read a line from the input file
+	OutFile.write(line)							##Write the line to the output file
+	line=line.rstrip()							##remove any trailing characters (DO WE NEED THIS?)
+	if line==";End of Gcode": break				##Stop if we find this line
+	content=line.split()						##Split the line at each space, stored in the array
+	try: tester=content[0]						##If this is a emthy line
+	except: continue							##Just ignore it and continue
+	if content[0]=="G0":						##If this line starts with a G0 command
+		coords=line								##Save the coords in case we need it later
 		ReturnCoordsFlag=1						##Flag that we have a location to return to 
+	if re.search("^;LAYER:",line):				##This would indicate a layer change
+		OutFile.write("G91\n" + 												##Use relative Positioning
+		"G1 F" + str(Feedrate) + " E-" + str(Retract) + "\n"					##Pull in filement
+		"G1 F" + str(Feedrate) + " Z" + str(Zhop) + "\n" + 						##Do a Z-hop
+		"G90\n" + 																##Use Absolute Positioning
+		"G1 F" + str(Feedrate) + " X" + str(PosX) + " Y" + str(PosY-PosSlowY) + "\n" + 	##Move almost to the edge
+		"G1 F" + str(FeedrateSlow) + " Y" + str(PosY) + "\n" + 					##Move (slowly) and push the button
+		"G0 F" + str(Feedrate) + "\n")											##Set the feedrate back (else the code seems to go slow??) NEEDS TO BE CHECKED
 		if ReturnCoordsFlag==1: OutFile.write(coords+"\n")						##Move back to last position before this code (if there is any)
+		OutFile.write("G91\n" + 												##Use relative Positioning
+		"G1 F" + str(Feedrate) + " E" + str(Retract) + "\n" 					##Z-hop back
+		"G1 F" + str(Feedrate) + " Z-" + str(Zhop) + "\n" + 					##Undo the Z hop
+		"G90\n" )																##Use Absolute Positioning
 print('Done!')
